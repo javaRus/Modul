35c90a371bc698af2fe802b54e9b0ded64f3a4d4
Ruslan <ruslan.bur@gmail.com>
2016-01-10 23:29:35 +0200

    Update MusicShop.java

--- src/third/MusicShop.java
+++ src/third/MusicShop.java
@@ -3,5 +3,5 @@ package third;
 /**
  * Created by Дима on 20.12.2015.
  */
-public class MusicShop extends MusicalInstrument {
+public class MusicShop extends MusicalInstrument { // Я думаю что  MusicShop может наследоваться от какого то абстрактного Shop ;)
 }
